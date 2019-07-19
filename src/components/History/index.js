a55bd23eb38bd9f17ff981b730af00fe2b9bdec3
JuliyaShirokova <juliasswork@gmail.com>
2019-07-19 20:29:01 +0300

    update

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Alert, Linking, Clipboard} from 'react-native';
+import {View, Text, TouchableOpacity, SectionList, StyleSheet, Alert, Linking, Clipboard} from 'react-native';
 import * as colors from '../../constants/colors';
 import * as fonts from '../../constants/fonts';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 class History extends Component{
+    constructor(props){
+        super(props);
     
-    getResult = () => {
+        this.state = {
+            selectedKey: '',
+            selectedData: '',
+        };
+        
+    }
+    
+    onItemPress = ( elKey, elData ) => {
+        const isSelected = elKey == this.state.selectedKey;
+        const valKey = ( isSelected ) ? '' : elKey; 
+        const valData = ( isSelected ) ? '' : elData; 
+        return  this.setState({
+            selectedKey: valKey,
+            selectedData: valData
+        })
+    }
+
+    getItemStyle = ( elKey ) => {
+    
+        const bColor = (elKey == this.state.selectedKey) ? '#919191' : 'transparent'; 
+        return {
+            backgroundColor: bColor
+        }
+    }
+
+    renderHistoryItem = (item, index) => {
+        const elKey = item.toString()+index;
+        return (
+            <View
+                key={index}
+                style={styles.historyItemHolder}
+            >
+                <TouchableOpacity
+                    style={[styles.historyItem, this.getItemStyle( elKey )]}
+                    onPress={() => this.onItemPress( elKey, item ) }
+                >
+                    <Text style={styles.historyItemText}>{ item }</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+    prepareData = ( arrData ) => {
+        const preparedData = arrData.map( (val, ind, arr) => (arr[ind]));
+        return [{ data: preparedData}] || [];
+    }
+
+    getList = () => {
         const { results } = this.props;
-        const last = results.length-1;
-        return results[last] || 'No results';
+        const list = (<SectionList
+                        renderItem={({item, index, section}) => this.renderHistoryItem(item, index)}
+                        sections={this.prepareData( results )}
+                        keyExtractor={(item, index) => item + index}
+                    />
+                )
+        return list || 'No results';
     }
     urlify = (text) => {
         var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
@@ -20,10 +74,7 @@ class History extends Component{
     }
 
     onMove = () => {
-        const { results } = this.props;
-        const last = results.length-1;
-        const curr = results[last] || null;
-        
+        const curr = this.state.selectedData;    
         const url = `${this.urlify(curr)}`;
         return Linking.canOpenURL(url)
             .then((supported) => {
@@ -36,31 +87,30 @@ class History extends Component{
             .catch((err) => console.err('An error occurred', err));
     }
 
-    copyToClipboard = async (text) => {
-        await Clipboard.setString(text);
-        Alert.alert('Copied to Clipboard!', text);
+    copyToClipboard = async () => {
+        const curr = this.state.selectedData;
+        await Clipboard.setString(curr);
+        Alert.alert('Copied to Clipboard!', curr);
     };
   
 
     render(){
-        const { results } = this.props;
-        const last = results.length-1;
-        const curr = results[last] || null;
-    
         return (
             <View
                 style={styles.container}
             >
                 <View
-                    style={styles.resultContent}
+                    style={styles.historyContent}
                 >
-                    <Text style={styles.resultText}>{this.getResult()}</Text>
+                    <ScrollView style={{flex: 1}}>
+                        {this.getList()}
+                    </ScrollView>
                 </View>
                 <View style={styles.actionButtons}>
                     <View style={style=styles.actionButtonHolder}>
                     
                         <TouchableOpacity
-                            onPress={() => this.copyToClipboard(curr)}
+                            onPress={() => this.copyToClipboard('text')}
                             style={styles.actionButton}
                         >
                             <Text style={styles.actionButtonText}>COPY</Text>
@@ -84,15 +134,22 @@ const styles=StyleSheet.create({
         flex: 1,
         justifyContent: 'space-between',
     },
-    resultContent: {
-        marginTop: 35,
+    historyContent: {
+        flex: 1,
+        marginTop: 24,
     },
-    resultText: {
+    historyItem: {
+        paddingHorizontal: 26,
+    },
+    historyItemText: {
         fontFamily: fonts.HelveticaNeue,
-        fontSize: 20,
+        fontSize: 16,
+        lineHeight: 23,
         color: colors.contentText,
+        paddingVertical: 8,
     },
     actionButtons: {
+        paddingHorizontal: 16,
         marginTop: 10,
         marginBottom: '10%',
         flexDirection: 'row',
--- src/components/Screens/HistoryScreen/index.js
+++ src/components/Screens/HistoryScreen/index.js
@@ -35,7 +35,6 @@ class HistoryScreen extends Component{
 const styles=StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 16,
     },
     menuContainer: {
         width: 30,
